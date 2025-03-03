2b22489421bf8d1899d5ad65c4ed6073cbae157f
ManuelCrispino <manuel.barman96@gmail.com>
2025-03-03 04:43:45 +0100

    getline

--- 14.  in-out/challange2/main.c
+++ 14.  in-out/challange2/main.c
@@ -12,21 +12,18 @@ int main (){
     return 1;
    };
    int lineCounter = 0 ;
-   while (fgets(input,sizeof(input),fr) != NULL){
-    for(int i = 0 ; input[i];i++){
-        if(input[i] == ' '|| input[i] == '\n'){
-           continue; // salta gli spazi e new line 
+   while (fgets(input, sizeof(input), fr) != NULL) {
+    int j = 0;
+    for (int i = 0; input[i] != '\0'; i++) {
+        if (input[i] != ' ' && input[i] != '\n') {
+            input[j] = toupper(input[i]);   
+            j++;
         };
-        input[i]=toupper(input[i]);
-        /* printf("%c\n",input[i]); */
-    }
+    };
+    input[j] = '\0'; // IMPORTANTE: termina la stringa modificata
     lineCounter++;
-    if (lineCounter < 10){
-    printf(" %d: %s",lineCounter,input);
-    }else{
-        printf("%d: %s",lineCounter,input);
+    printf("%2d: %s\n", lineCounter, input);
     }
-   }
    printf("\n"); 
    fclose(fr);
    return 0;
---
+++ 15.getline/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int freeBuffer (char *buffer,size_t bufsize,size_t characters){
+    printf("Memory allocated: %zu bytes\n", bufsize);
+    printf("Memory used: %zu bytes\n", characters);
+    printf("Memory used: %zu bit\n", characters*8);
+    printf("Memory remaining: %zu bytes\n", bufsize - characters);
+    free(buffer);
+    buffer=NULL;// riassegnare buffer Null evita imprevisti e protegge 
+    printf("buffer is now free\n");
+    return 0; };
+
+int main (){
+char *buffer = NULL;
+size_t bufsize = 32;
+size_t characters;
+
+buffer = (char *)malloc(bufsize * sizeof(char));
+
+if(buffer == NULL){
+    exit(1);
+};
+    printf("Type something\n");
+    characters =  getline(&buffer,&bufsize,stdin); 
+// getline() rialloca la memoria in automatico evitando bufoverflow ma puo creare problemi memoria . 
+    if(characters >= 32){
+        printf("too many characters error: overflow\n");
+        freeBuffer(buffer, bufsize, characters);
+        buffer = NULL; // Evita che venga liberato di nuovo
+        exit(1);
+    }
+    printf("\n%zu characters where read \n",characters);
+    printf("\nyou typed : %s\n",buffer);
+    if (buffer != NULL) { // Controllo per evitare doppio free
+        freeBuffer(buffer, bufsize, characters);
+    }
+};
