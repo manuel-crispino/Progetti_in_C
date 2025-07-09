88525f4c3b721917f3d9c772d6177ae04f8f1836
ManuelCrispino <manuel.barman96@gmail.com>
2025-07-09 18:34:18 +0200

    updt

--- 42-piscine/01/main.c
+++ 42-piscine/01/main.c
@@ -1,30 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mcrispino <mcripsino@42.at>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/5 9:42 by mcrispino              #+#    #+#             */
+/*   Created: 2025/07/9 18:42 by mcrispino             #+#    #+#             */
 /*   Updated:    time.    by                          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int ft_putc (char c){
-return write(0,&c,1);
-}
+int ft_putc(char c){
+   return write(1,&c,1);
+};
 
-void ft_print_alphabet (void){
-char alphabet = 'a';
-while(alphabet <= 'z'){
-   ft_putc(alphabet); 
-   alphabet++;
-}
-}
+void ft_print_comb2(){
+   int i = 0; 
 
-int main(void) {
-    ft_print_alphabet();
-    return 0;
+    while(i <= 99){
+        int j = i+1;
+     
+        while(j <= 99){
+             ft_putc(i / 10 + '0');
+             ft_putc(i % 10 + '0');
+             ft_putc(' ');
+             ft_putc(j / 10 + '0');
+             ft_putc(j % 10 + '0');
+             if(!(i==98 && j==99)){
+             write(1,", ",2);}
+             j++;
+        };
+          i++;
+    }
+}  
+
+int main (){
+    ft_print_comb2();
+    return 0; 
 }
