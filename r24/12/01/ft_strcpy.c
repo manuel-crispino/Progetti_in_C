def0b72679fe5995d774998cb2d64e90a02d8f46
ManuelCrispino <manuel.barman96@gmail.com>
2025-07-23 23:27:46 +0200

    updt

--- r24/12/01/ft_strcpy.c
+++ r24/12/01/ft_strcpy.c
@@ -1,20 +1,29 @@
-
-
-
-
-
 #include <stdio.h>
 
 char *ft_strcpy(char *dest, char *src)
 {
-	dest = src;
-	return (dest);
+	printf("*dest = %p\n",dest);
+	int ascii;
+	char *cpy = dest; //copy the pointer 
+	while (*src)
+	{
+		printf("*dest = %c == *src = %c\n",*dest,*src);
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	printf("before dest = %c\n",*dest);
+	*dest = '\0';// add end of word == null 
+	ascii = *dest;
+	printf("now dest = %d\n",ascii);
+	return (cpy);
 };
 
 int main (void)
 {
 	char a[] = "hello";
-	char *b = ft_strcpy(b,a);
-	printf("b = %s",b);
+	char b[100];
+	char *result = ft_strcpy(b,a);  
+	printf("result = %s",result);
 	return (0);
 }
