2bc6f1fd5fb304182563cfb09fa747c4c047ee5c
ManuelCrispino <manuel.barman96@gmail.com>
2025-07-30 22:54:47 +0200

    updt

---
+++ r24/03_01/04/main.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+char *ft_strstr(char *str, char *to_find)
+{
+	int i;
+	int len;
+	
+	len= 0; 
+	i=0;
+	while(to_find[len] != '\0')
+		len++; 
+	while(str[i] != '\0')
+	{
+				
+	}	
+}
+
+int main (void )
+{
+	return (0);
+}
+
